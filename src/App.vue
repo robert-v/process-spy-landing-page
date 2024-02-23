5a003f2c096727f07ec12d3ed587a1a08249d652
Robert Varga <robert.varga@sap.com>
2024-02-23 14:06:25 +0100

    updated responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,45 @@
 <template>
   <div class="relative overflow-hidden container mx-auto bg-emerald-100">
-    <header class="h-50 flex items-center">
+    <header class="h-50 flex items-center p-4">
       <div class="flex items-center grow">
         <img alt="Vue logo" src="./assets/logo.png" class="object-none object-center w-20 h-20 rounded-3xl">
         <h1 class="text-3xl font-bold">ProcessSpy</h1>
       </div>
-      <div class="flex flex-col">
-        <a href="/archive/ProcessSpy_1.2.1.zip" class="bg-emerald-500 px-4 w45 font-bold text-2xl mt-4" style=" padding:12px; color: white; border-radius:25px;">Download Free</a>
-        <span class="text-xs mt-1" style="font-size: 10pt">ProcessSpy 1.2.1, Apple Silicon and Intel</span>
+      <div class="flex flex-col text-center">
+        <a href="/archive/ProcessSpy_1.2.1.zip" class="bg-emerald-500 px-4 font-bold text-xl mt-4 md:text-2xl w-48 sm:w-64 place-self-end sm:place-self-auto" style=" padding:12px; color: white; border-radius:25px;">Download Free</a>
+        <span class="text-xs mt-1" style="font-size: 10pt">v1.2.1, Apple Silicon and Intel</span>
       </div>
     </header>
 
-    <div class="flex flex-row mt-10">
-       <div class="w-1/3 flex flex-col text-left mt-3">
+    <div class="md:flex mt-4">
+       <div class="md:shrink-0 md:w-1/3 p-4">
          <h1 class="text-6xl mt-6 text-left font-extrabold">View all macOS processes</h1>
          <p class="mt-8">
            View macOS processes with <b>command line arguments</b>. Other features:
          </p>
 
          <div class="justify-items-start mt-6 text-left">
-           <ul class="bg-blend-saturation list-decimal">
-             <li>highlight processes with custom colors based on search string</li>
-             <li>bring selected process to foreground</li>
-             <li>search process information online</li>
-             <li>open process path in Finder (where available)</li>
-             <li>copy process path (where available)</li>
-             <li>quit selected process</li>
-             <li>view open files</li>
-             <li>view open connections</li>
+           <ul class="bg-blend-saturation list-none">
+             <li><b>highlight</b> processes with custom colors based on search string</li>
+             <li><b>bring</b> selected process <b>to foreground</b></li>
+             <li><b>search</b> process information <b>online</b></li>
+             <li><b>open</b> process path in <b>Finder</b> (where available)</li>
+             <li><b>copy</b> process path (where available)</li>
+             <li><b>quit</b> selected process</li>
+             <li>view <b>open files</b></li>
+             <li>view <b>open connections</b></li>
            </ul>
          </div>
-
-
        </div>
-      <div class="mt-5 w-full flex-wrap text-left ml-12 h-30">
+
+      <div class="mt-5 w-full p-4">
         <img src="./assets/screenshot_1.png" class="rounded-2xl">
       </div>
 
     </div>
 
     <footer class="mt-20 mb-10 font-bold text-center">
-      <span class="m-10">© 2024 ProcessSpy</span>
+      <span class="p-10">© 2024 ProcessSpy</span>
       <span>Made by <a href="https://twitter.com/rob3rth" target="_blank">@rob3rth</a></span>
     </footer>
 
@@ -64,7 +63,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 </style>
